b13cf9430378f05f73ad6aa30365f439b4de2302
M1LO234 <milo.wrzesien@gmail.com>
2022-04-25 20:43:51 +0200

    Add: inital aggregation code

--- results_aggregation/aggregate_classification.py
+++ results_aggregation/aggregate_classification.py
@@ -1,7 +1,10 @@
+from email import header
 import os
 import json
+import pandas as pd
 
-from numpy import sort
+def float_to_num_with_comma(val):
+    return str(val).replace('.', ',')
 
 def iterate_over_dirs(main_dir, out_dir, range_dirs: list=None):
     list_dirs = os.listdir(main_dir)
@@ -18,25 +21,45 @@ def iterate_over_dirs(main_dir, out_dir, range_dirs: list=None):
             json_name = [f_name for f_name in os.listdir(conf_dir) if '.json' in f_name][0]
             json_path = os.path.join(conf_dir, json_name)
             curr_out_dir = os.path.join(out_dir, list_dirs[dir_id])
-            aggregate_from_json(json_path, curr_out_dir)
-            break
-        break
-
-
-
+            aggregate_from_json(json_path, curr_out_dir, conf_name)
 
 # todo: IN - json path, out_path, OUT - csv with all results, txt describing configurations
-def aggregate_from_json(json_path, output_path):
+def aggregate_from_json(json_path, output_path, configuration_name):
     with open(json_path, 'r') as f:
         con = json.load(f)
 
     out_csv_path = output_path+'.csv'
+    out_txt_path = output_path+'_configs.csv'
 
+    # save configuration description
+    conf_values = list(con['config'].keys())
+    cols_conf = ['conf']+conf_values
+    curr_conf_values = [configuration_name] + [str(con['config'][k]) for k in conf_values]
+    df = pd.DataFrame([curr_conf_values], columns=cols_conf)
+    df.to_csv(out_txt_path, mode='a', header=not os.path.exists(out_txt_path) , sep=';')
+
+    # return 
+    cols = ['conf', 'train file(s)', 'train class', 'test file', 'test class', 'mean rmse', 'mean mpe', 'mean max_pe']
     classes_tested = list(con['test results'].keys())
-    print(classes_tested)
-    # todo append results to csv
+    conf_results = []
+    class_of_train = con['files']['class']
+    train_files = ','.join(['_'+str(tr) for tr in con['files']['training']])
+    for class_n in classes_tested:
+        test_files_for_curr_class = list(con['test results'][class_n].keys())
+        for test_file in test_files_for_curr_class:
+            rmse = con['test results'][class_n][test_file]['rmse'] #todo: reading multiple values (min, mean, max)
+            mpe = con['test results'][class_n][test_file]['mpe']
+            max_pe = con['test results'][class_n][test_file]['max_pe']
+            curr_line = [configuration_name, train_files, class_of_train, test_file, \
+                class_n, float_to_num_with_comma(rmse), float_to_num_with_comma(mpe), \
+                    float_to_num_with_comma(max_pe)]
+            conf_results.append(curr_line)
+    conf_df = pd.DataFrame(conf_results, columns=cols)
+    conf_df.to_csv(out_csv_path, mode='a', header=not os.path.exists(out_csv_path), sep=';')
 
 
-out_dir = '/Users/miloszwrzesien/Development/cognitiveMaps/new_fcm_module/fcm_results/25_04_res'
+
+
+out_dir = '/Users/miloszwrzesien/Development/cognitiveMaps/new_fcm_module/fcm_results_agg'
 main_dir = '/Users/miloszwrzesien/Development/cognitiveMaps/new_fcm_module/fcm_results'
 iterate_over_dirs(main_dir, out_dir)
