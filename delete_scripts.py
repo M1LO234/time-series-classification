cd11a15ff80c9b37466f1ba12d586f51ae49033e
M1LO234 <milo.wrzesien@gmail.com>
2022-12-04 09:24:25 +0100

    Mod typo in delete_scripts

--- delete_scripts.py
+++ delete_scripts.py
@@ -4,4 +4,4 @@ for f in [s for s in os.listdir('./') if '.sh' in s]:
     os.remove(f)
 
 for f in [s for s in os.listdir('./run_scripts') if '.sh' in s]:
-    os.remove(f'./run_scripts{f}')
+    os.remove(f'./run_scripts/{f}')
